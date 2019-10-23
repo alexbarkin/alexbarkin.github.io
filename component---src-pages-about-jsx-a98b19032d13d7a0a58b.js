317e95bdce0ca1eeb7cf6f1b6948720bd9972608
Alex Barkin <alexbarkin@AlexsMacBookPro.local>
2019-10-23 19:55:51 -0400

    Updates

---
+++ component---src-pages-about-jsx-a98b19032d13d7a0a58b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{226:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),c=a(112),u=a(224);a(229);t.default=function(){return o.a.createElement(u.a,null,o.a.createElement("h1",{className:"about--title"},"About me"),o.a.createElement(c.a,{to:"/"},"Go to home page."))}}}]);
+//# sourceMappingURL=component---src-pages-about-jsx-a98b19032d13d7a0a58b.js.map
