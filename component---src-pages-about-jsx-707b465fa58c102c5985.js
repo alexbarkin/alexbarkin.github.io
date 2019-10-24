1c1ced1d4aa073c0e252f2585070256b9a378e67
Alex Barkin <alexbarkin@AlexsMacBookPro.local>
2019-10-23 20:08:35 -0400

    Updates

---
+++ component---src-pages-about-jsx-707b465fa58c102c5985.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{227:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),c=a(111),u=a(225);a(230);t.default=function(){return o.a.createElement(u.a,null,o.a.createElement("h1",{className:"about--title"},"About me"),o.a.createElement(c.a,{to:"/"},"Go to home page."))}}}]);
+//# sourceMappingURL=component---src-pages-about-jsx-707b465fa58c102c5985.js.map
---
+++ styles-c528be5986820ce8e72c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{230:function(n,o,w){},231:function(n,o,w){}}]);
+//# sourceMappingURL=styles-c528be5986820ce8e72c.js.map
