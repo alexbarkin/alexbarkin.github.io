df0efbe814aeeb4869ca0695fa3834adb33a5337
alexbarkin+ci-build <alexbarkin+ci-build@rogers.com>
2019-12-06 03:50:46 +0000

    Updates

--- component---src-pages-resume-jsx-bc8688c9b554d7a0f49e.js
+++ component---src-pages-resume-jsx-bc8688c9b554d7a0f49e.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[9],{"0yfb":function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),c=a.n(n),r=a("Wbzz"),u=a("L6Je"),l=a("qudm"),m=a.n(l);a("Vl0y");t.default=function(){return c.a.createElement(u.a,null,c.a.createElement("h1",null,"Resume"),c.a.createElement("embed",{src:m.a,className:"pdf--resume"}),c.a.createElement("br",null),c.a.createElement(r.Link,{to:"/About"},"Learn more about me!"))}},qudm:function(e,t,a){e.exports=a.p+"static/resume_alex-1877bf07d2c409a8bc0b327946bc268c.pdf"}}]);
-//# sourceMappingURL=component---src-pages-resume-jsx-819adbf80fb20eaf402e.js.map
+//# sourceMappingURL=component---src-pages-resume-jsx-bc8688c9b554d7a0f49e.js.map
